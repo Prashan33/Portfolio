c2ec4d294e9686a7cd8d87b15a4c4f9a584368ce
Prashan Adhikari <prashanadhikari2486@gmail.com>
2026-04-12 11:55:33 -0500

    made some changes in projects

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -20,17 +20,11 @@ const RecentProjects = () => {
           >
             <PinContainer title={item.liveLink} href={item.liveLink}>
               {/* Cover image */}
-              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[28vh] mb-4">
-                <div
-                  className="relative w-full h-full overflow-hidden lg:rounded-3xl"
-                  style={{ backgroundColor: "#13162D" }}
-                >
-                  <img src="/bg.png" alt="bgimg" />
-                </div>
+              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[28vh] mb-4 rounded-xl">
                 <img
                   src={item.img}
                   alt="cover"
-                  className="z-10 absolute bottom-0"
+                  className="w-full h-full object-cover object-top"
                 />
               </div>
 
